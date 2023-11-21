b1cedcb2f1a5b72d9ed0b4e165076de9d498182a
Adolf <adolfnyerere@gmail.com>
2023-11-21 15:07:48 +0300

    completed assignment

--- lib/app.py
+++ lib/app.py
@@ -1,7 +1,3 @@
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
-print("Hello world!")
+print("Hello World! Pass this test, please.", end="\n")
+
+
